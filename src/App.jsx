5ef590a56d8ee56422b27521f8f618287787b561
Tee-Mou <tennysonmorris@outlook.com>
2026-03-09 15:59:28 +0000

    Chnaged theming. Built UI for log-in system.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,72 @@
+import { use } from 'react';
 import './App.css';
 
-function Button({ id, link }) {
-  const handleClick = () => {
-    window.open(link);
-  };
+const idLinkMap = {
+  "gh": "https://github.com/Tee-Mou",
+  "li": "https://linkedin.com/in/tennyson-morris"
+}
+const idPlaceholderMap = {
+  "user": "Username",
+  "pass": "Password"
+}
+var user = null;
+var pass = null;
+var loginSuccess = false;
+
+function GuestGreeting() {
   return (
-    <button 
-    className='link_btn'
-    id={id}
-    onClick = {handleClick}>
-    </button>
+   <h1> Hey! We haven't met yet... </h1>
+  )
+}
+function LoginGreeting() {
+  return (
+   <h1> Welcome back {user}! </h1>
+  )
+}
+
+function Greeting() {
+  if (loginSuccess) {
+    return <LoginGreeting/>
+  } else {
+    return <GuestGreeting/>
+  }
+}
+
+function PersonalLink({id}) {
+  return (
+    <a className='link_btn' id={id} href={idLinkMap[id]}/>
   );
+}
+
+function LoginInput({id}) {
+  return (
+    <input type='text' className='login_input' id={id} placeholder={idPlaceholderMap[id]}/>
+  )
+}
+
+function LoginForm() {
+  return (
+    <form className='login_form'>
+      <LoginInput id='user'/>
+      <LoginInput id='pass'/>
+      <input className='login_submit' type='submit' value='Enter'/>
+    </form>
+  )
 }
 
 const App = () => {
   return (
     <div className="content">
-      <h1>Lemonade</h1>
-      <p>Check out what I've been doing.</p>
+      <Greeting/>
+      <p>Check out what I've been doing:</p>
       <div className="links_row1">
-        <Button 
-        id="gh"
-        link="https://github.com/Tee-Mou"/>
-        <Button 
-        id="li"
-        link="https://linkedin.com/in/tennyson-morris"/>
+        <PersonalLink 
+        id="gh"/>
+        <PersonalLink 
+        id="li"/>
       </div>
+      
+      <LoginForm/>
     </div>
   );
 };
