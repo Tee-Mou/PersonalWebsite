746a5ecaa7b2abf7f2af5c9b36aecdad6b3a1927
Tee-Mou <tennysonmorris@outlook.com>
2026-03-17 12:32:23 +0000

    Fixed bug where currentUser would be null and ternary would fail.

--- app/src/routes/adminRoute.jsx
+++ app/src/routes/adminRoute.jsx
@@ -3,9 +3,10 @@ import { useSelector } from 'react-redux';
 
 export const AdminRoute = ({ children }) => {
     const { currentUser, loading } = useSelector((state) => state.user);
-
+    var perms = "none";
     if (loading) return <div className="loading">Loading...</div>;
-    return currentUser.perms === "admin" ? children : <Navigate to="/" />;
+    if (currentUser) perms = currentUser.perms;
+    return currentUser?.perms === "admin" ? children : <Navigate to="/" />;
 }
 
 export default AdminRoute;
