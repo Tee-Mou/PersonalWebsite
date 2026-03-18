833125c08f38c7eb9f0a3a20b13f6f2f47bae93f
Tee-Mou <tennysonmorris@outlook.com>
2026-03-18 23:35:43 +0000

    Implemented navbar and UI for memeber dashboard.

--- app/src/pages/dashboards/member.jsx
+++ app/src/pages/dashboards/member.jsx
@@ -1,9 +1,63 @@
+import { useSelector, useDispatch } from "react-redux"
+
 import "./dashboard.css"
+
+const UserInfoWindow = () => {
+    const { currentUser, loading, err } = useSelector((state) => state.user)
+    if (loading) return <div>Loading... </div>
+    if (currentUser) { 
+        return (
+            <div className="user-info-window">
+                <table className="user-table">
+                    <tbody>
+                        <tr>
+                            <th scope="row" align="right">User ID:</th>
+                            <td>{currentUser._id}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" align="right">Name:</th>
+                            <td>{currentUser.user}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row" align="right">Level:</th>
+                            <td>{currentUser.perms}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    } 
+}   
+
+const UserWindow = () => {
+    const deleteUser = () => { return }
+    const changeUsername = () => { return }
+    const changePassword = () => { return }
+    return (
+        <div className="user-manager">
+            <UserInfoWindow/>
+            <div className="manager-buttons">
+                <button className="admin-button" onClick={deleteUser}>
+                    Delete Account
+                </button>
+                <button className="admin-button" onClick={changeUsername}>
+                    Change Username
+                </button>
+                <button className="admin-button" onClick={changePassword}>
+                    Change Password
+                </button>
+            </div>
+        </div>
+    )
+}
 
 export const MemberDashboard = () => {
     return (
         <div className="dashboard-page">
             <h1> Member Dashboard </h1>
+            <div className="Dashboard">
+                <UserWindow/>
+            </div>
         </div>
     )
 };
--- app/src/pages/sidebar/sidebar.jsx
+++ app/src/pages/sidebar/sidebar.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logOutUser } from "../../contexts/userSlice";
 
 import "./sidebar.css"
 
 export const Sidebar = () => {
+    const dispatch = useDispatch()
+    const logout = () => {
+        dispatch(logOutUser());
+    }
     return (
         <div className="sidebar">
-            <Link className="nav-icon" to={"/"}>
-                <p>Hey</p>
+            <Link className="nav-icon" id="home" to={"/"}>
+            </Link>
+            <Link className="nav-icon" id="logout" onMouseDown={logout}>
             </Link>
             <Link className="nav-icon" to={"/"}>
-                <p>Hey</p>
-            </Link>
-            <Link className="nav-icon" to={"/"}>
-                <p>Hey</p>
             </Link>
         </div>
     )
